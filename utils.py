cd3f4aaa412e2daa67d41b1ba8628a94313efc05
sgse <sgse.vlad@gmail.com>
2023-11-27 21:09:31 +0300

    Функция для чтения json-файлов

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
 # Утилиты для чтения файлов
+import json
+
+
+def load_operations(data: str):
+    """
+    Открывает json-файл на чтение
+    :param data: путь к файлу
+    :return: json-файл с операциями
+    """
+    with open(data, "r", encoding="utf-8") as file:
+        return json.load(file)
